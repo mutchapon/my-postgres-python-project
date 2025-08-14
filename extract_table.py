16d8129aa89c29019542f785d969ffa1af583876
Mutchapon Chummai <mutchach@yipintsoigroup.com>
2025-08-14 14:49:39 +0700

    add extract table(name_date)

--- extract_table.py
+++ extract_table.py
@@ -1,10 +1,14 @@
 import os
+from datetime import datetime
 from utils.export_csv import export_to_csv
 
-if __name__ == "__main__":
+def main():
+    today = datetime.today().strftime("%Y-%m-%d")
     sql_path = os.path.join("sql", "select_users.sql")
-    csv_path = os.path.join("output", "users_export.csv")
-
-    os.makedirs("output", exist_ok=True)  # สร้างโฟลเดอร์ถ้ายังไม่มี
-
+    csv_filename = f"user_{today}.csv"
+    csv_path = os.path.join("output", csv_filename)
+    os.makedirs("output", exist_ok=True)
+    print(f"Exporting to: {csv_path}")
     export_to_csv(sql_path, csv_path)
+if __name__ == "__main__":
+    main()
